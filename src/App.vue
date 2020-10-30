f0e274b705c9b4c1961588e7188e96e3c0f0e68f
Lucas Passos <lucas.depassos@yahoo.com.br>
2020-10-30 16:37:05 -0300

    first_commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="cyan lighten-1"
+      dark
+    >
+     <img src="./assets/aprovalogo.png" alt=""
+     style="max-width:200px;"
+     >
+    </v-app-bar>
+  
+    <v-main>
+      <Form />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Form from './components/Form'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    Form
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Form.vue
@@ -0,0 +1,84 @@
+<template>
+  <v-form
+    ref="form"
+    v-model="valid"
+    lazy-validation
+    style="margin-left: 40%;"
+  >
+    <v-text-field
+      v-model="name"
+      :counter="10"
+      :rules="nameRules"
+      label="Nome"
+      required
+      style="max-width: 250px;"
+    ></v-text-field>
+
+    <v-text-field
+      v-model="email"
+      :rules="emailRules"
+      label="E-mail"
+      required
+      style="max-width: 250px;"
+
+    ></v-text-field>  
+
+    <v-checkbox
+      v-model="checkbox"
+      :rules="[v => !!v || 'Você deve concordar para prosseguir!']"
+      label="Você concorda?"
+      required
+    ></v-checkbox>
+
+    <v-btn
+      :disabled="!valid"
+      color="success"
+      class="mr-4"
+      @click="validate"
+    >
+      Validar
+    </v-btn>
+
+    <v-btn
+      color="error"
+      class="mr-4"
+      @click="reset"
+    >
+      Reiniciar
+    </v-btn>
+
+   
+  </v-form>
+</template>
+
+
+<script>
+  export default {
+    data: () => ({
+      valid: true,
+      name: '',
+      nameRules: [
+        v => !!v || 'Nome é um campo obrigatório',       
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail é um campo obrigatório',
+        v => /.+@.+\..+/.test(v) || 'Precisa ser um email válido',
+      ],
+      select: null,
+      
+    }),
+
+    methods: {
+      validate () {
+        this.$refs.form.validate()
+      },
+      reset () {
+        this.$refs.form.reset()
+      },
+      resetValidation () {
+        this.$refs.form.resetValidation()
+      },
+    },
+  }
+</script>
